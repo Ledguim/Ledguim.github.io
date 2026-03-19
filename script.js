82f5e627e20e90ee403ae5e1a2923cc620a32da5
Lewi <redcarstern@gmail.com>
2026-03-19 18:49:50 -0300

    Lab 1 Página Personal

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('changeBtn');
+const input = document.getElementById('userInput');
+const text = document.getElementById('dynamicText');
+
+btn.addEventListener('click', () => {
+    if (input.value.trim() !== "") {
+        text.innerText = input.value;
+        input.value = ""; 
+    } else {
+        alert("Please write something first!");
+    }
+});
